d3a4af148dc17b4c94346be719308f5daafe01a2
Manuel Henriquez <manuelhm1993@gmail.com>
2025-02-15 20:59:12 -0400

    Uso de plantillas blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,36 @@
-<x-app-layout>
+@extends('layouts.app') {{-- Heredar de la plantilla layouts.app --}}
+
+@section('title', env('app'))
+
+{{-- Directiva para código estático y se puede usar en varios lugares --}}
+@push('css')
+    <style>
+        body {
+            background-color: #f3f3f3;
+        }
+    </style>
+@endpush
+
+@push('css')
+    <style>
+        body {
+            color: red;
+        }
+    </style>
+@endpush
+
+{{-- Indicar el contenido de la sección --}}
+@section('content')
     <div class="max-w-4xl mx-auto px-4">
         <x-alert2 type="info" class="mb-4">
             {{-- Contenido variable del componente --}}
             <x-slot:title>
                 Título
             </x-slot>
-    
+
             Contenido del alert
         </x-alert2>
-    
+
         <p>Hola mundo</p>
-    </div>
-</x-app-layout>
+    </div>    
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    {{-- Valor por defecto de la sección --}}
+    <title>@yield('title', 'Laravel 11')</title>
+
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
+    {{-- fontawesome --}}
+    {{-- Tipografía --}}
+
+    {{-- Directiva para código estático y se puede usar en varios lugares --}}
+    @stack('css')
+</head>
+<body>
+    <header></header>
+
+    @yield('content') {{-- Contenido variable --}}
+
+    <footer></footer>
+
+    @stack('js')
+</body>
+</html>
