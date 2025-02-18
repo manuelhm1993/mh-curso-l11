a8624f45064aef9a005036066ca8e45f54ff582d
Manuel Henriquez <manuelhm1993@gmail.com>
2025-02-17 23:04:23 -0400

    Routing avanzado - se aprendió el uso de las rutas con nombre para agilizar el proceso de mantenimiento, las rutas de recursos que permiten simplificar el código CRUD y la forma de modificar prefijos y parámetros

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,11 @@ use App\Models\Post;
 //------------------------------------ Si el controlador es de tipo __invoke no es necesario especificarlo
 Route::get('/', HomeController::class);
 
-//------------------------------------ Agrupar y nombrar rutas
-Route::prefix('posts')->name('posts.')->controller(PostController::class)->group(function () {
+//------------------------------------ Ruta de recursos normal
+Route::resource('posts', PostController::class);
+
+//------------------------------------ Ruta de recursos manual
+/*Route::prefix('articulos')->name('posts.')->controller(PostController::class)->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('/create', 'create')->name('create');
     Route::post('/', 'store')->name('store');
@@ -20,3 +23,22 @@ Route::prefix('posts')->name('posts.')->controller(PostController::class)->group
     Route::put('/{post}', 'update')->name('update');
     Route::delete('/{post}', 'destroy')->name('destroy');
 });
+*/
+
+//------------------------------------ Ruta de recursos para cambiar los prefijos
+/*Route::resource('articulos', PostController::class)->names([
+    'index'   => 'posts.index',
+    'create'  => 'posts.create',
+    'store'   => 'posts.store',
+    'show'    => 'posts.show',
+    'edit'    => 'posts.edit',
+    'update'  => 'posts.update',
+    'destroy' => 'posts.destroy',
+])->parameters([
+    'articulos' => 'post' 
+]);*/
+
+//------------------------------------ Ruta de recursos para cambiar los prefijos en una línea
+/*Route::resource('articulos', PostController::class)->names('posts')->parameters([
+    'articulos' => 'post' 
+]);*/
