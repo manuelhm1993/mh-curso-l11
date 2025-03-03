0bb4c6b72207e5ee9a36b0e05dcbd0ab87966f77
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-03 18:06:14 -0400

    Uso de la relación uno a uno y el eager loading para cargar toda la data desde el controlador

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Phone;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,14 +15,16 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //------------------------------------ Crear el usuario principal
-        User::factory()->create([
-            'name'     => 'Manuel Henriquez',
-            'email'    => 'manuelhm1993@gmail.com',
-            'password' => bcrypt('12345678'),
-        ]);
+        //------------------------------------ Crear el usuario principal con su teléfono asociado
+        User::factory()
+            ->has(Phone::factory()->count(1))
+            ->create([
+                'name'     => 'Manuel Henriquez',
+                'email'    => 'manuelhm1993@gmail.com',
+                'password' => bcrypt('12345678'),
+            ]);
 
-        //------------------------------------ Crear 2 usuarios de prueba
-        User::factory(10)->create();
+        //------------------------------------ Crear 10 usuarios de prueba con su teléfono asociado
+        User::factory(10)->has(Phone::factory()->count(1), 'phone')->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,38 +5,16 @@ use Illuminate\Support\Facades\Route;
 //------------------------------------ Importa el controlador para poder usarlo
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
-use App\Models\Post;
+use App\Http\Controllers\UserController;
 
 //------------------------------------ Si el controlador es de tipo __invoke no es necesario especificarlo
 Route::get('/', HomeController::class);
 
-//------------------------------------ Ruta de recursos normal
-Route::resource('posts', PostController::class);
-
-//------------------------------------ Ruta de recursos manual
-/*Route::prefix('articulos')->name('posts.')->controller(PostController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/create', 'create')->name('create');
-    Route::post('/', 'store')->name('store');
-    Route::get('/{post}', 'show')->name('show');
-    Route::get('/{post}/edit', 'edit')->name('edit');
-    Route::put('/{post}', 'update')->name('update');
-    Route::delete('/{post}', 'destroy')->name('destroy');
-});
-*/
-
-//------------------------------------ Ruta de recursos para cambiar los prefijos
-/*Route::resource('articulos', PostController::class)->names([
-    'index'   => 'posts.index',
-    'create'  => 'posts.create',
-    'store'   => 'posts.store',
-    'show'    => 'posts.show',
-    'edit'    => 'posts.edit',
-    'update'  => 'posts.update',
-    'destroy' => 'posts.destroy',
-])->parameters([
-    'articulos' => 'post' 
-]);*/
+//------------------------------------ Controladores de recursos
+Route::resources([
+    'users' => UserController::class,
+    'posts' => PostController::class,
+]);
 
 //------------------------------------ Ruta de recursos para cambiar los prefijos en una línea
 /*Route::resource('articulos', PostController::class)->names('posts')->parameters([
