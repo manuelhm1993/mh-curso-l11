d39cfb0b676decefa2e996570b1774a5ba074c78
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-02 23:33:19 -0400

    Personalizar la dirección del remitente, el nombre y el asunto

--- app/Mail/PostCreated.php
+++ app/Mail/PostCreated.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -27,7 +28,8 @@ class PostCreated extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Post Created',
+            from: new Address('administracion@mhenriquez.com', 'Manuel Henriquez'), //Remitente
+            subject: 'Post por aprobar', //Asunto
         );
     }
 
